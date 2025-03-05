c19862d52c316890b2e64ed9c61ba730e3e01f5e
FuwaFuwinski <182450639+FuwinskiFuwa@users.noreply.github.com>
2025-03-05 06:32:31 +0300

    Initial upload

---
+++ js/fetchContacts.js
@@ -0,0 +1,30 @@
+fetch('data/contacts.json')
+.then(response => response.json())
+.then(contacts => {
+    // Функция для отображения контактов
+    const contactsDiv = document.querySelector('.contacts-wrapper'); 
+    
+    contacts.forEach(contact => {
+        // Создаем элемент для контакта
+        const contactElement = document.createElement('div');
+        contactElement.classList.add('contact');
+        
+        // Добавляем иконку
+        const iconElement = document.createElement('div');
+        iconElement.classList.add('icon');
+        iconElement.innerHTML = contact.icon;
+        contactElement.appendChild(iconElement);
+
+        // Добавляем текст
+        const textElement = document.createElement('span');
+        textElement.classList.add('text');
+        textElement.textContent = contact.text;
+        contactElement.appendChild(textElement);
+
+        // Добавляем контакт в список
+        contactsDiv.appendChild(contactElement);
+    });
+})
+.catch(error => console.error('Yikes. Ошибка при загрузке contacts.json:', error));
+
+
